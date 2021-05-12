6582ba69b2a6875dddeae287ae07d8ef19088b20
Tara Harwood <tara.m.harwood@gmail.com>
2021-05-11 22:01:06 -0700

    mongo db inital setup

--- server.js
+++ server.js
@@ -3,22 +3,30 @@ const bodyParser= require('body-parser')
 const app = express();
 const MongoClient = require('mongodb').MongoClient;
 
-app.use(bodyParser.urlencoded({ extended: true }))
 
-app.listen(9000, function() {
-    console.log('listening on 9000')
+
+connectionString="mongodb+srv://elixa:LF26!.AaqDDCHh!@cluster0.24bp7.mongodb.net/elixa-db?retryWrites=true&w=majority"
+
+MongoClient.connect(connectionString, { useUnifiedTopology: true })
+  .then(client => {
+    console.log('Connected to Database')
+    const db = client.db('elixa-db')
+
+    app.use(bodyParser.urlencoded({ extended: true }))
+
+    app.listen(9000, function() {
+        console.log('listening on 9000')
+    })
+
+    app.get('/', (req, res) => {
+        res.sendFile(__dirname + '/index.html')
+    })
+
+    app.post('/quotes', (req, res) => {
+        console.log(req.body)
+    })
+
 })
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html')
-})
-
-app.post('/quotes', (req, res) => {
-    console.log(req.body)
-})
-
-MongoClient.connect('mongodb-connection-string', (err, client) => {
-  // ... do something here
-})
-
+.catch(console.error)
 console.log("Hello Node World")
